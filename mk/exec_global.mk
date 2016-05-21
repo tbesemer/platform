202c71ece3bb35a2e5802df8da01740e122d7561
Thomas Besemer <tbesemer@gmail.com>
2016-05-21 15:29:00 -0700

    Ongoing.

--- mk/exec_global.mk
+++ mk/exec_global.mk
@@ -2,6 +2,8 @@ $(ODIR)/%.o: %.c
 	${CC} ${EXEC_CFLAGS} ${INCLUDES} -c -o $@ $<
 	$(SHELL) -ec '${CC} ${EXEC_CFLAGS} ${INCLUDES} -MM $< | sed '\"s/$*.o/${ODIR}\\/$*.o/\"' > $(ODIR)/$*.d'
 
+EXEC_NAME=${BDIR}/${EXEC}
+
 ${EXEC_NAME}:   $(OBJ)
 	${CC} -o $@ $^
 
